f9cd6a9a64e19d439355c19fce4675ffd65aea2d
Monde Lacanlalay <monde.lacanlalay@ocomsoft.com>
2019-12-09 19:26:59 +0800

    added token endpoint

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import include, path
 
 from rest_framework.routers import DefaultRouter
+from rest_framework.authtoken import views
 
 from users.api.views import UserViewSet
 from expenses.api.views import ExpenseTrackerViewSet, TypeOfExpensesViewSet
@@ -14,5 +15,6 @@ router.register(r'type_of_expenses', TypeOfExpensesViewSet)
 urlpatterns = [
     path('', include(router.urls)),
     path('rest-auth/', include('rest_auth.urls')),
-    path('rest-auth/registration/', include('rest_auth.registration.urls'))
+    path('rest-auth/registration/', include('rest_auth.registration.urls')),
+    path('api-token-auth/', views.obtain_auth_token)
 ]
--- budget_tracker/settings.py
+++ budget_tracker/settings.py
@@ -71,7 +71,12 @@ REST_FRAMEWORK = {
     # or allow read-only access for unauthenticated users.
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated'
-    ]
+    ],
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+
+    ],
 }
 
 AUTH_USER_MODEL = 'users.User'
