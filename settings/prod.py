509c124df6fa50e61bc6ff8512818425a48b3f09
Monde Lacanlalay <monde.lacanlalay@gmail.com>
2020-03-20 19:05:25 +0800

    changed the allowed hosts for prod

--- settings/prod.py
+++ settings/prod.py
@@ -2,7 +2,7 @@ from .base import *
 
 DEBUG = False
 
-ALLOWED_HOSTS = ['.pythonanywhere.com']
+ALLOWED_HOSTS = ['.herokuapp.com']
 
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
