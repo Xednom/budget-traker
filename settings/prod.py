072cbed160340d61a28781e2ee18b6b098eefb1e
Richmond Lacanlalay <monde.lacanlalay@ocomsoft.com>
2020-05-20 23:17:52 +0800

    build and added the empty staticfiles dir

--- settings/prod.py
+++ settings/prod.py
@@ -9,28 +9,80 @@ ALLOWED_HOSTS = ['.herokuapp.com', '.pythonanywhere.com']
 
 DATABASES = {
     'default': {
-        # 'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        # 'NAME': 'd63f7luvevhghb',
-        # 'USER': 'tnyyrlaxfryaom',
-        # 'PASSWORD': '685a83048898676366f5a789ef0a14c521bcbefc7954e39e2b722cfcd1a65358',
-        # 'HOST': 'ec2-54-197-48-79.compute-1.amazonaws.com',
-        # 'PORT': '5432',
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'mabtracker$budget',
-        'USER': 'mabtracker',
-        'PASSWORD': 'fishmond22',
-        'HOST': 'mabtracker.mysql.pythonanywhere-services.com',
-        'PORT': '3306',
-        'OPTIONS': {
-         'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
-        },
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'd6rlu2380djf5',
+        'USER': 'tbcdgzqqhxeizc ',
+        'PASSWORD': '5955ea9a7e8402fbac58600856aeabb479459595a7956672b2a1a9bc17ddfa13',
+        'HOST': 'ec2-46-137-84-140.eu-west-1.compute.amazonaws.com',
+        'PORT': '5432',
+        # 'ENGINE': 'django.db.backends.mysql',
+        # 'NAME': 'mabtracker$budget',
+        # 'USER': 'mabtracker',
+        # 'PASSWORD': 'fishmond22',
+        # 'HOST': 'mabtracker.mysql.pythonanywhere-services.com',
+        # 'PORT': '3306',
+        # 'OPTIONS': {
+        #  'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+        # },
     }
 }
 
+LOGGING = {
+	'version': 1,
+	'disable_existing_loggers': False,
+	'filters': {
+		'require_debug_false': {
+			'()': 'django.utils.log.RequireDebugFalse',
+		},
+		'require_debug_true': {
+			'()': 'django.utils.log.RequireDebugTrue',
+		},
+	},
+	'formatters': {
+		'django.server': {
+			'()': 'django.utils.log.ServerFormatter',
+			'format': '[%(server_time)s] %(asctime)s %(message)s',
+		}
+	},
+	'handlers': {
+		'console': {
+			'level': 'INFO',
+			'filters': ['require_debug_true'],
+			'class': 'logging.StreamHandler',
+		},
+		'console_debug_false': {
+			'level': 'ERROR',
+			'filters': ['require_debug_false'],
+			'class': 'logging.StreamHandler',
+		},
+		'django.server': {
+			'level': 'INFO',
+			'class': 'logging.StreamHandler',
+			'formatter': 'django.server',
+		},
+		# 'mail_admins': {
+		# 	'level': 'ERROR',
+		# 	'filters': ['require_debug_false'],
+		# 	'class': 'django.utils.log.AdminEmailHandler'
+		# }
+	},
+	'loggers': {
+		'django': {
+			'handlers': ['console', 'console_debug_false'],
+			'level': 'INFO',
+		},
+		'django.server': {
+			'handlers': ['django.server'],
+			'level': 'INFO',
+			'propagate': False,
+		}
+	}
+}
 
 # see https://docs.djangoproject.com/en/2.1/howto/static-files/
 STATIC_ROOT = (
     os.path.join(BASE_DIR, 'staticfiles')
+    os.path.join(BASE_DIR, 'frontend/dist')
 )
 
 # SSL/TLS SETTINGS FOR DJANGO
