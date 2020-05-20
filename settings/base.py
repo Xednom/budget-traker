2ac32fd6cf6a3c9b5f5a7622144cc9a5c56e7634
Richmond Lacanlalay <monde.lacanlalay@ocomsoft.com>
2020-05-21 01:43:21 +0800

    added staticfiles

--- settings/base.py
+++ settings/base.py
@@ -154,8 +154,6 @@ LOGOUT_REDIRECT_URL = '/'
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "frontend/dist")
 ]
--- settings/prod.py
+++ settings/prod.py
@@ -79,6 +79,9 @@ LOGGING = {
 	}
 }
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+
 # see https://docs.djangoproject.com/en/2.1/howto/static-files/
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'staticfiles'),
